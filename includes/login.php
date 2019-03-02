531bd99fdbf6a269b7a7dd4af02a3a3a6ae11547
Anthuan Vasquez <me@anthuanvasquez.net>
2019-03-02 16:28:11 -0400

    Removed framework dependencies

--- includes/login.php
+++ includes/login.php
@@ -25,13 +25,13 @@ function anva_custom_login_logo_url_title() {
  */
 function anva_custom_login_footer() {
 	
-	$url    = 'http://anthuanvasquez.net/';
-	$author = __( 'Anthuan Vasquez', 'anva-custom-login' );
+	$url    = 'https://anthuanvasquez.net/';
+	$author = __( 'Anthuan Vásquez', 'anva-custom-login' );
 	
 	printf(
 		'<div class="login-footer"><p class="login-footer__copyrights">&copy %1$s %2$s %3$s.</p></div>',
 		date('Y'),
 		apply_filters( 'anva_custom_login_author', sprintf( '<a class="login-footer__link" href="%1$s">%2$s</a>', esc_url( $url ), esc_html( $author ) ) ),
-		esc_html( anva_get_local( 'footer_copyright' ) )
+		esc_html__( 'All Rights Reserved' , 'anva-custom-login' )
 	);
 }
